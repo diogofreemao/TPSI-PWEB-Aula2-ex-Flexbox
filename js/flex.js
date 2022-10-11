cf3c65b24a018762bbb65d15700c0b4de8705c45
Diogo Machado <diogo.prata.massano@gmail.com>
2022-10-11 17:29:56 +0100

    justify, align e banners

--- js/flex.js
+++ js/flex.js
@@ -5,3 +5,29 @@ function flex(valor) {
 function wrap(valor) {
 	document.getElementById('container').style.flexWrap=valor;
 }
+function justify(valor) {
+	document.getElementById('container').style.flexJustify=valor;
+}
+function align(valor) {
+	document.getElementById('container').style.alignItems=valor;
+}
+function slide(){
+	document.getElementById('publicidade').src="../img/8af.jpg";
+	document.getElementById('descricao').innerHTML="lol";
+	setTimeout("slide2()", 2000)
+}
+function slide2(){
+	document.getElementById('publicidade').src="../img/8af2.jpg";
+	document.getElementById('descricao').innerHTML="lol2";
+	setTimeout("slide3()", 2000)
+}
+function slide3(){
+	document.getElementById('publicidade').src="../img/8af3.jpg";
+	document.getElementById('descricao').innerHTML="lol3";
+	setTimeout("slide4()", 2000)
+}
+function slide4(){
+	document.getElementById('publicidade').src="../img/8af4.jpg";
+	document.getElementById('descricao').innerHTML="lol4";
+	setTimeout("slide()", 2000)
+}
